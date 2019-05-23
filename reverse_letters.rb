ee18227b2b7d94f69f626496ddd3a0c9c5d2d3f9
Kandis Flick <kandisflick@gmail.com>
2019-05-22 19:38:46 -0700

    initialize new repository

---
+++ reverse_letters.rb
@@ -0,0 +1,16 @@
+#takes in an array of 2 letters and returns an array of 2 letters
+def reverse_letters(word)
+    reversed_letters = []
+    index = word.length - 1 #since we start at 0
+    
+    loop do
+        reversed_letters << word[index]
+        index -= 1
+        break if index < 0
+    end
+
+    reversed_letters
+    
+end
+
+print reverse_letters("happy")
